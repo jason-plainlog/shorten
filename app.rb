2c0f298f86fcca78ba8011fbb7ffd7c8ccedaf34
Jason Hsieh <jason.plainlog@gmail.com>
2018-01-22 18:57:20 +0800

    Bugs Fixed and Status Added

--- app.rb
+++ app.rb
@@ -15,17 +15,24 @@ class MyApp < Sinatra::Application
 		return string
 	end
 
-	get '/' do
+	before do
 		@key = ""
+		@dbsize = redis.dbsize
+	end
 
+	get '/' do
 		erb :index
 	end
 
 	post '/' do
+		halt erb(:not_url) if params['url'][0..7] != "https://" and params['url'][0..6] != "http://"
+		halt erb(:too_short) if params['url'][0..(url.length-1)] == url
+
 		@key = hash(4)
 		@key = hash(4) while redis.exists(@key)
 
 		redis.hmset(@key, "url", params['url'], "password", params['password'])
+		redis.expire(@key, 2592000)
 		puts "#{@key} linked to #{params['url']} with password #{params['password']}"
 
 		erb :index
